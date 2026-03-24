0e19bada4ed7b48ab80aa274beed0e88c2d85a9d
notbryan2006 <notbryandaniel@gmail.com>
2026-03-24 10:19:59 -0700

    se implemento el primer GET en una REST API

--- index.js
+++ index.js
@@ -1,10 +1,23 @@
 const express = require('express');
 const app = express();
 
+// Ruta principal
 app.get('/', (req, res) => {
-  res.send('Hola mundo desde Express');
+  res.send('API funcionando');
 });
 
+// Nueva ruta /usuario
+app.get('/usuario', (req, res) => {
+  const usuario = {
+    id: 1,
+    nombre: 'Bryan',
+    rol: 'Administrador'
+  };
+
+  res.json(usuario);
+});
+
+// Servidor
 app.listen(3000, () => {
   console.log('Servidor corriendo en http://localhost:3000');
 });
