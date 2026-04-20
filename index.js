7bf10b197102d8689b8a450e37434033e7e48a4b
notbryan2006 <notbryandaniel@gmail.com>
2026-04-20 10:15:49 -0700

    Endpoint POST para insertar alumnos y materias

--- index.js
+++ index.js
@@ -41,6 +41,43 @@ app.post('/alumnos', async (req, res) => {
   }
 });
 
+
+app.get('/materias', async (req, res) => {
+  try {
+    const resultado = await pool.query('SELECT * FROM materia');
+    res.json(resultado.rows);
+  } catch (error) {
+    console.error('Error al consultar materias:', error);
+    res.status(500).json({ error: 'Error al obtener las materias' });
+  }
+});
+
+app.post('/materias', async (req, res) => {
+  try {
+    const { nombre, semestre, creditos } = req.body;
+
+    
+    if (!nombre || semestre == null || creditos == null) {
+      return res.status(400).json({
+        error: 'Todos los campos son obligatorios'
+      });
+    }
+
+    const resultado = await pool.query(
+      'INSERT INTO materia (nombre, semestre, creditos) VALUES ($1, $2, $3) RETURNING *',
+      [nombre, semestre, creditos]
+    );
+
+    res.status(201).json({
+      mensaje: 'Materia insertada correctamente',
+      materia: resultado.rows[0]
+    });
+
+  } catch (error) {
+    console.error('Error al insertar materia:', error);
+    res.status(500).json({ error: 'Error al insertar la materia' });
+  }
+});
 app.listen(3000, () => {
   console.log('Servidor corriendo en http://localhost:3000');
 });
